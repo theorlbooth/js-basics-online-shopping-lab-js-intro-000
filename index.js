36ea5652ef464e4e79e393002c3b105196218c61
Theo Booth <theorlbooth@googlemail.com>
2020-03-03 10:40:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ function viewCart() {
 }
 
 function total() {
-  var prices = []
   var sum = 0
   for (let i = 0; i < cart.length; i += 1) {
+    var prices = []
     prices.push(cart[i].itemPrice)
     sum += prices[i]
   }
