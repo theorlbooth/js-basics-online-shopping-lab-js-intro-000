6619035a84414e1852499c2cc553675aa149d716
Theo Booth <theorlbooth@googlemail.com>
2020-03-03 12:15:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -76,9 +76,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
     ttl_cost = total()
-    for (let i = 0; i < cart.length; i+= 1) {
-      removeFromCart(cart[i].itemName)
-    }
+    cart = []
     return `Your total cost is $${ttl_cost}, which will be charged to the card ${cardNumber}.`
   }
 }
